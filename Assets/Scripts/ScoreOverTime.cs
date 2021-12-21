2990f9b27c95310d42e0cd322bbde2e3120cde45
Simge Erdil <siisi4officiel@gmail.com>
2021-12-21 03:27:04 +0100

    Time Score

---
+++ Assets/Scripts/ScoreOverTime.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreOverTime : MonoBehaviour
+{
+    private float timer;
+    private int score;
+    public Text ScoreText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > 1f){
+            score +=1;
+            ScoreText.text = score.ToString();
+            //timer = 0;
+        }
+
+    }
+}
