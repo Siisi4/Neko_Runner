cdccd998d35521154e0c180497f0e87db6e2395c
Simge Erdil <siisi4officiel@gmail.com>
2021-12-21 04:15:24 +0100

    reward et score

---
+++ Assets/Scripts/FishReward.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FishReward : MonoBehaviour
+{
+    public Text Fish;
+    private int rewards;
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.CompareTag("Player")){
+            Debug.Log("Fish obtained");
+            rewards +=1;
+            Fish.text = rewards.ToString();
+        }
+    }
+    
+}
