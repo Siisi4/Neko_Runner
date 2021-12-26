b6d0641fda5607d26a80805c4c7e3b4f96a63ab7
Simge Erdil <siisi4officiel@gmail.com>
2021-12-26 06:45:54 +0100

    reward bugfix

--- Assets/Scripts/FishReward.cs
+++ Assets/Scripts/FishReward.cs
@@ -8,10 +8,11 @@ public class FishReward : MonoBehaviour
     public Text Fish;
     private int rewards;
     void OnTriggerEnter2D(Collider2D other) {
-        if(other.gameObject.CompareTag("Player")){
+        if(other.gameObject.CompareTag("reward")){
             Debug.Log("Fish obtained");
             rewards +=1;
             Fish.text = rewards.ToString();
+            Destroy(other.gameObject);
         }
     }
     
