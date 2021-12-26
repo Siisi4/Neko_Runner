b1b51d36b9ac3913193f21a819fff452e455e822
Simge Erdil <siisi4officiel@gmail.com>
2021-12-26 05:41:16 +0100

    menus base

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    public void PlayButton(){
+        SceneManager.LoadScene("LevelA");
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+        Debug.Log("Application.Quit");
+    }
+}
